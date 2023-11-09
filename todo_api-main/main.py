61923b03b6775c6b538f5a241692425816dc6488
Tony Wu <xiaowu@berkeley.edu>
2023-11-09 12:39:49 -0800

    Update database support

--- todo_api-main/main.py
+++ todo_api-main/main.py
@@ -5,6 +5,31 @@ from db.database import engine
 from routers import auth, todos, admin
 from routers.payment import router as payment_router
 from routers.generics import router as generics_router
+import sqlite3
+
+# Connect to SQLite3 database. This will create the file if it does not exist.
+conn = sqlite3.connect('my_database.db')
+
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS transactions (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    amount DECIMAL(10, 2),
+    status TEXT,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+''')
+cursor.execute('''
+INSERT INTO transactions (user_id, amount, status) VALUES (?,?,?)
+''', (123, 200.00, 'processed'))
+conn.commit()
+cursor.execute('SELECT * FROM transactions')
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+conn.close()
+
 
 # application
 app = FastAPI()
